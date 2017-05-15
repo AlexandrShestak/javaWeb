9c30848ac5fa941cda5bb46423e6a2c4054ec39c
AlexandrShestak <shestakalexandr@mail.ru>
2017-05-15 22:59:55 +0300

    Migrate for java based configuration approach for MessageSource

---
+++ src/main/java/com/shestakam/configuration/MessageConfiguration.java
@@ -0,0 +1,23 @@
+package com.shestakam.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages.index",
+                "messages.index",
+                "messages.hello",
+                "messages.news",
+                "messages.user",
+                "messages.users");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
